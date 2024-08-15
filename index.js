8360f97561b887b1522cc5f0148efc2e686d39d4
Pragyan <ghimirepragyan100@gmail.com>
2024-08-15 09:37:29 +0545

    create ejs ui

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
-// for static contents
-app.use(express.static("public"));
+
+app.use(express.static("public"));// for static contents
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
     res.render("index.ejs");
-})
+});
+
+app.get("/create",(req,res)=>{
+    res.render("create.ejs");
+});
+
+app.post("/post",(req,res)=>{
+    res.render("index.ejs");
+    console.log(req.body["title"]);
+});
+
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 });
