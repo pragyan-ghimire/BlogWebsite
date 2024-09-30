b9160b9f1d2e96fa0ed858b18c1e605db6868a3c
pragyan <ghimirepragyan100@gmail.com>
2024-09-30 21:51:03 +0545

    readme file added and formatted documents

---
+++ README.md
@@ -0,0 +1,101 @@
+
+
+![blogLogo](https://github.com/user-attachments/assets/3210bd57-3357-4f68-a878-98d933548cd9)
+
+## Table of Contents
+
+- [About The Project](#about-the-project)
+- [Built With](#built-with)
+- [Features](#features)
+- [Getting Started](#getting-started)
+- [Prerequisites](#prerequisites)
+- [Installation](#installation)
+- [Usage](#usage)
+## About The Project
+![Screenshot (23)](https://github.com/user-attachments/assets/dfc94505-9700-4f8b-8e5e-e9cef1a412ec)
+BlogsTalks is a website that allows you to create, post, update and delete blogs. This is a capstone project that I created to learn concepts of EJS and Expressjs.
+### Built With
+These are the frameworks/libraries used to create this project. 
+
+* [![Bootstrap](https://img.shields.io/badge/Bootstrap-v5.3.3-563d7c)](https://getbootstrap.com)
+* [![Expressjs](https://img.shields.io/badge/Express-v4.19.02-eeeeee)](https://expressjs.com)
+* [![Node.js](https://img.shields.io/badge/Node.js-v22.9.0-417e38)](https://nodejs.org/en)
+* [![body-parser](https://img.shields.io/badge/body--parser-v1.20.2-blue)](https://www.npmjs.com/package/body-parser)
+
+### Features
+
+- Create blog posts
+- Edit blog posts
+- Delete blog posts
+- View Blog post in simpler format
+
+
+
+
+## Getting Started
+This section will guide you through setting up and running the blog website on your local machine.
+### Prerequisites
+Ensure you have the following installed on your local development machine:
+* Node.js (v18 or above) – [Download here](https://nodejs.org/en/download/package-manager)
+* npm (Node package manager) – Comes with Node.js
+* Code editor or IDE. Recommended - [Visual Studio Code](https://code.visualstudio.com/download)
+### Installation
+
+Clone the project
+
+```bash
+  git clone https://github.com/pragyan-ghimire/BlogWebsite.git
+```
+
+Go to the project directory
+
+```bash
+  cd /path/BlogWebsite
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  nodemon index.js
+```
+
+
+## Usage
+
+Once the server is running, you can use the blog website as follows:
+
+### 1. Access the Homepage
+
+- Open your browser and go to `http://localhost:3000`.
+- The homepage displays options to navigate to home page, create a blog post and view it in simpler format.
+![image](https://github.com/user-attachments/assets/c0ca7684-2749-43e2-af36-081ffeda983b)
+
+
+### 2. Create a New Blog Post
+
+- Click on "Create" button to create new post.
+- Fill in the title and content.
+- Click "Post" to publish your post.
+![image](https://github.com/user-attachments/assets/dd9ccf89-23f8-44ef-b93c-989e113bb9f1)
+
+
+### 3. Edit Blog Posts
+
+- **Edit**: Go to one of your posts and click "Edit" to make changes. After editing, update the post.
+![image](https://github.com/user-attachments/assets/2c32a7a0-e475-4a81-8c11-d47522b1b19b)
+
+### 4. Delete Blog Posts
+
+- **Delete**: Click "Delete" on one of your posts to permanently remove it.
+![image](https://github.com/user-attachments/assets/2387965a-a15e-4ba9-8efd-2cf5b60c6884)
+
+
+### 5. View in simpler format
+- **View**: Click "View" to view the posted blogs in simpler format.
+![image](https://github.com/user-attachments/assets/7e27bb71-b893-4c6f-8756-fd76b4c726be)
--- index.js
+++ index.js
@@ -6,68 +6,68 @@ const port = 3000;
 let titles = [];
 let contents = [];
 
-app.use(express.static("public"));// for static contents
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public")); // for static contents
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",(req,res)=>{
-    res.render("index.ejs",{
-        title:titles,
-        content:contents
-    });
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    title: titles,
+    content: contents,
+  });
 });
 
-app.get("/create",(req,res)=>{
-    res.render("create.ejs");
+app.get("/create", (req, res) => {
+  res.render("create.ejs");
 });
 
-app.post("/",(req,res)=>{
-    titles.push(req.body["title"]);
-    contents.push(req.body["content"]);
-    res.render("index.ejs",{
-        title:titles,
-        content:contents,
-    });
-    // console.log(titles);
+app.post("/", (req, res) => {
+  titles.push(req.body["title"]);
+  contents.push(req.body["content"]);
+  res.render("index.ejs", {
+    title: titles,
+    content: contents,
+  });
+  // console.log(titles);
 });
 
-app.get("/edit/:index",(req,res)=>{
-    const index = req.params.index;
-    res.render("create.ejs",{
-        titleValue:titles[index],
-        contentValue:contents[index],
-        indexValue:index,
-    });
+app.get("/edit/:index", (req, res) => {
+  const index = req.params.index;
+  res.render("create.ejs", {
+    titleValue: titles[index],
+    contentValue: contents[index],
+    indexValue: index,
+  });
 });
 
-app.get("/view",(req,res)=>{
-    res.render("index.ejs",{
-        view : true,
-        title:titles,
-    });
+app.get("/view", (req, res) => {
+  res.render("index.ejs", {
+    view: true,
+    title: titles,
+  });
 });
 
-app.post("/update/:index",(req,res)=>{
-    const index = req.params.index;
-    titles.splice(index, 1, req.body["title"]);  // The first parameter is the index, the second is how many elements to remove, and the third is the new value.
-    contents.splice(index, 1, req.body["content"]);  // The first parameter is the index, the second is how many elements to remove, and the third is the new value.
+app.post("/update/:index", (req, res) => {
+  const index = req.params.index;
+  titles.splice(index, 1, req.body["title"]); // The first parameter is the index, the second is how many elements to remove, and the third is the new value.
+  contents.splice(index, 1, req.body["content"]); // The first parameter is the index, the second is how many elements to remove, and the third is the new value.
 
-    // contents[index] = req.body["content"];
-    // console.log(titles);
-    res.render("index.ejs",{
-        title:titles,
-        content:contents,
-    });
+  // contents[index] = req.body["content"];
+  // console.log(titles);
+  res.render("index.ejs", {
+    title: titles,
+    content: contents,
+  });
 });
-app.post("/delete/:index",(req,res)=>{
-    const index = req.params.index;
-    titles.splice(index,1); // remove 1 element at index
-    contents.splice(index,1);
-    // console.log(titles);
-    res.render("index.ejs",{
-        title:titles,
-        content:contents,
-    });
+app.post("/delete/:index", (req, res) => {
+  const index = req.params.index;
+  titles.splice(index, 1); // remove 1 element at index
+  contents.splice(index, 1);
+  // console.log(titles);
+  res.render("index.ejs", {
+    title: titles,
+    content: contents,
+  });
 });
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
